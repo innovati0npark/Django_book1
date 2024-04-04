fe5e50e8c6d10751f33996ed7f9a8b91575cd965
InnovationPARK <innovation0220@gmail.com>
2024-04-04 13:04:58 +0900

    bookmark one

---
+++ bookmark/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from bookmark.models import Bookmark
+
+
+# Register your models here.
+# admin.site.register(Bookmark)
+@admin.register(Bookmark)
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display=('id', 'title', 'url')
+
+
+
